45954842c75c6d72096eb7a2c9c48318437cd6cf
Zhang Zheng <2385747226@qq.com>
2024-03-03 21:09:34 +0800

    template class for serial class

--- src/main.cc
+++ src/main.cc
@@ -4,10 +4,11 @@
 #include "robot_controller.hpp"
 #include "serial_interface.hpp"
 #include "thread"
+#include "types.hpp"
 
 int main(int argc, char **argv) {
     Robot::Robot_ctrl robot;
-    Hardware::Serial_interface serial;
+    Hardware::Serial_interface<Types::ReceivePacket> serial;
 
     Io::Io_handler io(robot.robot_set);
 
@@ -34,8 +35,7 @@ int main(int argc, char **argv) {
     robot.start();
 
     std::thread io_thread(&Io::Io_handler::task, &io);
-    std::thread serial_thread(&Hardware::Serial_interface::task, &serial);
-    std::thread copy_thread(copySerialData);
+    std::thread serial_thread(&Hardware::Serial_interface<Types::ReceivePacket>::task, &serial);
 
     io_thread.join();
     return 0;
